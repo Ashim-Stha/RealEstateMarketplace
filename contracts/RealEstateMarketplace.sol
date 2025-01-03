d87eeaaf9439a7336065d7239db7838719bbff0e
Ashim <ashimshrestha2384@gmail.com>
2025-01-03 18:40:50 +0545

    cancelListing

--- contracts/RealEstateMarketplace.sol
+++ contracts/RealEstateMarketplace.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 error RealEstateMarketplace__PriceMustnotBeZero();
 error RealEstateMarketplace__NotApprovedForMarketplace();
@@ -11,7 +12,7 @@ error RealEstateMarketplace__NotOwner();
 error RealEstateMarketplace__NotListed(address assestAddress, uint256 tokenId);
 error RealEstateMarketplace__PriceNotMet(address assestAddress, uint256 tokenId, uint256 price);
 
-contract RealEstateMarketplace {
+contract RealEstateMarketplace is ReentrancyGuard {
     struct Listing {
         uint256 price;
         address seller;
@@ -19,7 +20,7 @@ contract RealEstateMarketplace {
 
     //Events
     event AssestListed(address indexed seller, address indexed assestAddress, uint256 indexed tokenId, uint256 price);
-    event AssestBought(address indexed buyer,address indexed assestAddress,address indexed tokenId,uint256 price);
+    event AssestBought(address indexed buyer, address indexed assestAddress, uint256 indexed tokenId, uint256 price);
 
     mapping(address => mapping(uint256 => Listing)) private listings;
     mapping(address => uint256) private proceeds;
@@ -68,7 +69,12 @@ contract RealEstateMarketplace {
         emit AssestListed(msg.sender, assestAddress, tokenId, price);
     }
 
-    function buyAssest(assestAddress, tokenId);(address assestAddress, uint256 tokenId) external payable isListed(assestAddress, tokenId) {
+    function buyAssest(address assestAddress, uint256 tokenId)
+        external
+        payable
+        nonReentrant
+        isListed(assestAddress, tokenId)
+    {
         Listing memory listedAssest = listings[assestAddress][tokenId];
         if (msg.value < listedAssest.price) {
             revert RealEstateMarketplace__PriceNotMet(assestAddress, tokenId, listedAssest.price);
@@ -76,6 +82,6 @@ contract RealEstateMarketplace {
         proceeds[listedAssest.seller] = proceeds[listedAssest.seller] + msg.value;
         delete(listings[assestAddress][tokenId]);
         IERC721(assestAddress).safeTransferFrom(listedAssest.seller, msg.sender, tokenId);
-        emit ItemBought(msg.sender,assestAddress,tokenId, listedAssest.price);
+        emit AssestBought(msg.sender, assestAddress, tokenId, listedAssest.price);
     }
 }
