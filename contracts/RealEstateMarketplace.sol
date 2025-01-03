890d9982fb1c71857f5084868ebb24f28d569869
Ashim <ashimshrestha2384@gmail.com>
2025-01-03 18:13:40 +0545

    writing buyAssest

--- contracts/RealEstateMarketplace.sol
+++ contracts/RealEstateMarketplace.sol
@@ -8,6 +8,8 @@ error RealEstateMarketplace__PriceMustnotBeZero();
 error RealEstateMarketplace__NotApprovedForMarketplace();
 error RealEstateMarketplace__AlreadyListed(address assestAddress, uint256 tokenId);
 error RealEstateMarketplace__NotOwner();
+error RealEstateMarketplace__NotListed(address assestAddress, uint256 tokenId);
+error RealEstateMarketplace__PriceNotMet(address assestAddress, uint256 tokenId, uint256 price);
 
 contract RealEstateMarketplace {
     struct Listing {
@@ -16,9 +18,11 @@ contract RealEstateMarketplace {
     }
 
     //Events
-    event AssestListed(address indexed seller, address indexed assestAdrress, uint256 indexed tokenId, uint256 price);
+    event AssestListed(address indexed seller, address indexed assestAddress, uint256 indexed tokenId, uint256 price);
+    event AssestBought(address indexed buyer,address indexed assestAddress,address indexed tokenId,uint256 price);
 
     mapping(address => mapping(uint256 => Listing)) private listings;
+    mapping(address => uint256) private proceeds;
 
     //modifier
     modifier notListed(address assestAddress, uint256 tokenId, address owner) {
@@ -34,6 +38,14 @@ contract RealEstateMarketplace {
         address owner = assest.ownerOf(tokenId);
         if (spender != owner) {
             revert RealEstateMarketplace__NotOwner();
+        }
+        _;
+    }
+
+    modifier isListed(address assestAddress, uint256 tokenId) {
+        Listing memory listing = listings[assestAddress][tokenId];
+        if (listing.price <= 0) {
+            revert RealEstateMarketplace__NotListed(assestAddress, tokenId);
         }
         _;
     }
@@ -54,5 +66,16 @@ contract RealEstateMarketplace {
 
         listings[assestAddress][tokenId] = Listing(price, msg.sender);
         emit AssestListed(msg.sender, assestAddress, tokenId, price);
+    }
+
+    function buyAssest(assestAddress, tokenId);(address assestAddress, uint256 tokenId) external payable isListed(assestAddress, tokenId) {
+        Listing memory listedAssest = listings[assestAddress][tokenId];
+        if (msg.value < listedAssest.price) {
+            revert RealEstateMarketplace__PriceNotMet(assestAddress, tokenId, listedAssest.price);
+        }
+        proceeds[listedAssest.seller] = proceeds[listedAssest.seller] + msg.value;
+        delete(listings[assestAddress][tokenId]);
+        IERC721(assestAddress).safeTransferFrom(listedAssest.seller, msg.sender, tokenId);
+        emit ItemBought(msg.sender,assestAddress,tokenId, listedAssest.price);
     }
 }
