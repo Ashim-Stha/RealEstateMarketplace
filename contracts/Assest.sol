3563f5554b72a28dcb2a3a0537e3322068238ea0
Ashim <ashimshrestha2384@gmail.com>
2025-01-04 10:28:17 +0545

    modified Assest.sol

--- contracts/Assest.sol
+++ contracts/Assest.sol
@@ -1,5 +1,4 @@
 //SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -7,16 +6,21 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract Assest is ERC721URIStorage {
     uint256 private tokenCounter;
     mapping(string => uint256[]) private citizenshipIdToAssestIds;
-    //mapping(string => bool) private mintedAssestId;
 
     constructor() ERC721("Assest", "A") {}
 
     function mintNft(string memory tokenUri, string memory citizenshipId) public {
-        //require(!mintedAssestId[citizenshipId], "Token already minted for this assest");
         tokenCounter += 1;
         _safeMint(msg.sender, tokenCounter);
         _setTokenURI(tokenCounter, tokenUri);
         citizenshipIdToAssestIds[citizenshipId].push(tokenCounter);
-        //mintedAssestId[citizenshipId] = true;
+    }
+
+    function getTokensByCitizenshipId(string memory citizenshipId) external view returns (uint256[] memory tokenIds) {
+        return citizenshipIdToAssestIds[citizenshipId];
+    }
+
+    function getTokenUri(uint256 tokenId) public view returns (string memory) {
+        return tokenURI(tokenId);
     }
 }
